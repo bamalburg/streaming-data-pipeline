fc194829e51c59c9f9e24d83101befae0df31fab
Ben Malburg <bamalburg@gmail.com>
2022-04-25 18:51:14 -0400

    finished challenges 4 and 5

--- hbase-hello-world/src/main/scala/com/labs1904/hwe/MyApp.scala
+++ hbase-hello-world/src/main/scala/com/labs1904/hwe/MyApp.scala
@@ -1,14 +1,20 @@
 package com.labs1904.hwe
 
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get, Put, Scan}
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Delete, Get, Put, Scan}
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.logging.log4j.{LogManager, Logger}
 
+import java.util
+import java.util.List
 import scala.collection.convert.ImplicitConversions.`iterator asScala`
 
 object MyApp {
   lazy val logger: Logger = LogManager.getLogger(this.getClass)
+
+  // Optional helper functions
+//  implicit def stringToBytes(str: String): Array[Byte] = Bytes.toBytes(str)
+  implicit def bytesToString(bytes: Array[Byte]): String = Bytes.toString(bytes)
 
   def main(args: Array[String]): Unit = {
     logger.info("MyApp starting...")
@@ -40,24 +46,53 @@ object MyApp {
 //      println(favoriteColor)
 
       // Challenge 3
+      // https://hbase.apache.org/2.1/apidocs/org/apache/hadoop/hbase/client/Scan.html
 //      val scan = new Scan().withStartRow(Bytes.toBytes("10000001")).withStopRow(Bytes.toBytes("10006001"))
 //      val scanner = table.getScanner(scan).iterator()
-//      println(scanner.size)
-////      https://hbase.apache.org/2.1/apidocs/org/apache/hadoop/hbase/client/Scan.html
+//      println(scanner.size) // had to import something for this; there's probably a better way to solve this challenge...?
 
 
       // Challenge 4
-//      Challenge #4: Delete the user with ID = 99 that we inserted earlier.
-//        Delete
-//      val myDeleteObject = new Delete("rowkey") //Deletes entire row
+      // https://hbase.apache.org/2.1/apidocs/org/apache/hadoop/hbase/client/Delete.html
+//      val myDeleteObject = new Delete(Bytes.toBytes("99"))
+//      val resultDelete = table.delete(myDeleteObject)
 //
-//      https://hbase.apache.org/2.1/apidocs/org/apache/hadoop/hbase/client/Delete.html
-        // START HERE //
+//      // check that it was deleted; if so, you should get "null" returned, I think
+//      val get = new Get(Bytes.toBytes("99"))
+//      val resultGet = table.get(get)
+//      val favoriteColor = Bytes.toString(resultGet.getValue(Bytes.toBytes("f1"), Bytes.toBytes("favorite_color")))
+//      println(favoriteColor)
 
 
+      // Challenge 5:
+      // There is also an operation that returns multiple results in a single HBase "Get" operation.
+      // Write a single HBase call that returns the email addresses for the following 5 users:
+      // 9005729, 500600, 30059640, 6005263, 800182 (Hint: Look at the Javadoc for "Table")
+      // https://hbase.apache.org/2.1/apidocs/org/apache/hadoop/hbase/client/Table.html
+      // default Result[] 	get(List<Get> gets)
+      // Extracts specified cells from the given rows, as a batch.
 
+      // probably better / more succint way to write this code
+      val get1 = new Get(Bytes.toBytes("9005729"))
+      val get2 = new Get(Bytes.toBytes("500600"))
+      val get3 = new Get(Bytes.toBytes("30059640"))
+      val get4 = new Get(Bytes.toBytes("6005263"))
+      val get5 = new Get(Bytes.toBytes("800182"))
+      val gets: util.List[Get] = new util.ArrayList[Get]() // confused about util.List vs util.ArrayList
 
+      gets.add(get1)
+      gets.add(get2)
+      gets.add(get3)
+      gets.add(get4)
+      gets.add(get5)
 
+      val result = table.get(gets)
+
+      // make sure I got the results
+      val userEmail_9005729 = Bytes.toString(result(0).getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
+      println(userEmail_9005729)
+      val userEmail_800182 = Bytes.toString(result(4).getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
+      println(userEmail_800182)
 
 
       //      logger.debug(result)
