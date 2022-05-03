6a851070d6d90e884700ff4d873f7b8c785eda4a
Ben Malburg <bamalburg@gmail.com>
2022-05-03 02:31:04 +0100

    step 3 initial draft

--- streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
+++ streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
@@ -30,6 +30,32 @@ case class Review(
   review_date: String
 )
 
+case class EnrichedReview(
+   marketplace: String,
+   customer_id: Int,
+   review_id: String,
+   product_id: String,
+   product_parent: Int,
+   product_title: String,
+   product_category: String,
+   star_rating: Int,
+   helpful_votes: Int,
+   total_votes: Int,
+   vine: String,
+   verified_purchase: String,
+   review_headline: String,
+   review_body: String,
+   review_date: String,
+   name: String,
+   username: String,
+   mail: String,
+   sex: String,
+   birthdate: String)
+
+// may need these for later steps
+// case class User()
+// case class EnrichedUser()
+
 
 object StreamingPipeline {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
@@ -58,10 +84,10 @@ object StreamingPipeline {
         .master("local[*]")
         .getOrCreate()
 
-      val conf = HBaseConfiguration.create()
-      conf.set("hbase.zookeeper.quorum", "hbase02.hourswith.expert:2181")
-      val connection = ConnectionFactory.createConnection(conf)
-      val table = connection.getTable(TableName.valueOf("bmalburg:users"))
+//      val conf = HBaseConfiguration.create()
+//      conf.set("hbase.zookeeper.quorum", "hbase02.hourswith.expert:2181")
+//      val connection = ConnectionFactory.createConnection(conf)
+//      val table = connection.getTable(TableName.valueOf("bmalburg:users"))
 
       import spark.implicits._
 
@@ -91,10 +117,50 @@ object StreamingPipeline {
           row(12), row(13), row(14)
         ))
 
+//      val query = reviews.writeStream
+//        .outputMode(OutputMode.Append())
+//        .format("console")
+//        .option("truncate", false)
+//        .trigger(Trigger.ProcessingTime("5 seconds"))
+//        .start()
 
 
+      // [START HERE]
+      // Step 3
+//      Use the customer_id contained within the review message to lookup corresponding user data in HBase.
+//        Construct a HBase get request for every review message. The customer_id corresponds to a HBase rowkey.
+//        Tip: Open up a connection per partition, instead of per row
 
-      val query = reviews.writeStream
+      val customers = reviews.mapPartitions(partition => {
+        val conf = HBaseConfiguration.create()
+        conf.set("hbase.zookeeper.quorum", "hbase02.hourswith.expert:2181")
+        val connection = ConnectionFactory.createConnection(conf)
+        val table = connection.getTable(TableName.valueOf("bmalburg:users"))
+
+        val iter = partition.map(row => {
+
+          val customerToGet = new Get(Bytes.toBytes(row.customer_id)).addFamily(Bytes.toBytes("f1"))
+          val customer = table.get(customerToGet)
+
+          val name = Bytes.toString(customer.getValue(Bytes.toBytes("f1"), Bytes.toBytes("name")))
+          val username = Bytes.toString(customer.getValue(Bytes.toBytes("f1"), Bytes.toBytes("username")))
+          val email = Bytes.toString(customer.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
+          val sex = Bytes.toString(customer.getValue(Bytes.toBytes("f1"), Bytes.toBytes("sex")))
+          val birthdate = Bytes.toString(customer.getValue(Bytes.toBytes("f1"), Bytes.toBytes("birthdate")))
+
+          EnrichedReview(row.marketplace, row.customer_id, row.review_id, row.product_id, row.product_parent,
+            row.product_title, row.product_category, row.star_rating, row.helpful_votes, row.total_votes,
+            row.vine, row.verified_purchase, row.review_headline, row.review_body, row.review_date,
+            name, username, email, sex, birthdate)
+
+        }).toList.iterator
+
+        connection.close()
+        iter
+      })
+
+
+      val query = customers.writeStream
         .outputMode(OutputMode.Append())
         .format("console")
         .option("truncate", false)
@@ -102,26 +168,9 @@ object StreamingPipeline {
         .start()
 
 
-
-
-      // [START HERE]
-
-//      Use the customer_id contained within the review message to lookup corresponding user data in HBase.
-//        Construct a HBase get request for every review message. The customer_id corresponds to a HBase rowkey.
-//        Tip: Open up a connection per partition, instead of per row
-
-      // Just trying to get a single one to print...not sure why it's not working
-      // Maybe this is a bad approach and I should instead just try to get all the info somehow,
-      // as part of the streaming, and print to console along the way to check if I'm doing it right,
-      // and then do whatever else comes after that?
-//      val customerToGet = new Get(Bytes.toBytes("24617080"))
-//      val result = table.get(customerToGet)
-//      val test = Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
-//      println(test)
-
-
-
-
+      // Step 4
+//      Join the review data with the user data into a Scala case class.
+//      Create a new case class that holds information for the review data and its corresponding user data. Verify your joined data by running the application and outputting via the console sink.
 
 
       // Write output to HDFS
